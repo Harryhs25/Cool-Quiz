a7059060780094dc384c7c106c0e081b8962c268
Harryhs25 <87052254+Harryhs25@users.noreply.github.com>
2021-10-22 01:17:09 +0000

    got rid of imported fonts as they removed the casual like look of the game changed questions back to cooking related questions after i got my friend to look at a bug where the questions wouldn't display on screen. Will now start work on the highscore documents. bugs encountered here will be left in the ReadMe. also removed duplicate script from bug fixing

--- game.js
+++ game.js
@@ -10,35 +10,40 @@ let score = 0
 let questionCounter = 0
 let availableQuestions = []
 
-  {
-    question: "What is the first thing you should do when preparing a meal?",
-    choice1: 'Play with your pet(s)'
-    choice2: 'Wash your hands'
-    choice3: 'Prepare your ingrediants'
-    choice4: 'Go on a walk'
-    answer: 2,
-  }
-      question2: "What does marinating do to meat?",
-    choice1: 'Adds different flavours to the meat'
-    choice2: 'Makes the meat cook faster'
-    choice3: 'Makes the meat look nice'
-    choice4: 'Makes the meat look presentable'
-    answer: 1,
-  }
-      question3: "What makes a cake turn brown?",
-    choice1: 'Someone defecated on the cake :o'
-    choice2: 'The eggs combining with other baked ingrediants'
-    choice3: 'Baking soda chemical reaction'
-    choice4: 'Brown Sugar'
-    answer: 3,
-  }
-      question4: "Bobotie is the national dish of which country?",
-    choice1: 'South Africa'
-    choice2: 'Ethiopia'
-    choice3: 'Kenya'
-    choice4: 'Chad'
-    answer: 1,
-  }
+let questions = [
+    {
+        question: 'What is the first thing you should do when preparing a meal?',
+        choice1: 'Play with your pet(s)',
+        choice2: 'Wash your hands',
+        choice3: 'Prepare your ingrediants',
+        choice4: 'Go on a walk',
+        answer: 2,
+    },
+    {
+        question:
+            "What does marinating do to meat?",
+        choice1: "Adds different flavours to the meat",
+        choice2: "Makes the meat cook more evenly",
+        choice3: "makes the meat look nice",
+        choice4: "None of the above",
+        answer: 1,
+    },
+    {
+        question: "What makes a cake turn brown?",
+        choice1: "Someone defecated on the cake :o",
+        choice2: "The eggs combining with other ingrediants",
+        choice3: "baking soda chemical reaction",
+        choice4: "brown sugar",
+        answer: 3,
+    },
+    {
+        question: "Bobotie is the national dish of which country?",
+        choice1: "South Africa",
+        choice2: "Ethiopia",
+        choice3: "Kenya",
+        choice4: "Chad",
+        answer: 1,
+    }
 ]
 
 const SCORE_POINTS = 100
